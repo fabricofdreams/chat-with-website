a53eba93c988cb629a0afbc6db446c033c0ec538
Fernando Robledo <fernando.robledoruiz@gmail.com>
2024-02-12 15:04:50 -0500

    Emulate the response

--- src/app.py
+++ src/app.py
@@ -1,18 +1,25 @@
 import streamlit as st
 
+
+def get_response(user_input):
+    return "I don't know."
+
+
+# App configuration
 st.set_page_config(page_title='Chat with Websites', page_icon='🤖')
 st.title('Chat with Websites')
 
+# Sidebar
 with st.sidebar:
     st.header('Settings')
     website_url = st.text_input('Website URL')
 
+# User input
 user_query = st.chat_input('Type a message')
 
 if user_query is not None and user_query != '':
-
+    response = get_response(user_query)
     with st.chat_message('Human'):
         st.write(user_query)
-
     with st.chat_message('AI'):
-        st.write('I don\'t know.')
+        st.write(response)
