3d992286503f6a247582a14f05c9b16740855233
Fernando Robledo <fernando.robledoruiz@gmail.com>
2024-02-12 15:35:20 -0500

    Show conversation

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
 
 
 def get_response(user_input):
@@ -8,6 +9,11 @@ def get_response(user_input):
 # App configuration
 st.set_page_config(page_title='Chat with Websites', page_icon='🤖')
 st.title('Chat with Websites')
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content='Hi! How can I help you?')
+    ]
 
 # Sidebar
 with st.sidebar:
@@ -19,7 +25,15 @@ user_query = st.chat_input('Type a message')
 
 if user_query is not None and user_query != '':
     response = get_response(user_query)
-    with st.chat_message('Human'):
-        st.write(user_query)
-    with st.chat_message('AI'):
-        st.write(response)
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+    st.session_state.chat_history.append(AIMessage(content=response))
+
+
+# Conversation
+for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message('AI'):
+            st.write(message.content)
+    elif isinstance(message, HumanMessage):
+        with st.chat_message('Human'):
+            st.write(message.content)
